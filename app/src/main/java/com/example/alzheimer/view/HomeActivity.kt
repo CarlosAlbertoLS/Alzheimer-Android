48bd4944a9d00a9b5f68514ff501e3acf8b92ec0
Carlos Lopez <clopezs1100@alumno.ipn.mx>
2022-03-28 21:34:19 -0600

    Implementacion de NavigationDrawer

--- app/src/main/java/com/example/alzheimer/view/HomeActivity.kt
+++ app/src/main/java/com/example/alzheimer/view/HomeActivity.kt
@@ -1,26 +1,78 @@
 package com.example.alzheimer.view
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.alzheimer.R
 import com.example.alzheimer.databinding.ActivityHomeBinding
+import com.example.alzheimer.databinding.AppBarMainBinding
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.firestore.FirebaseFirestore
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer:DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+
     private lateinit var binding: ActivityHomeBinding
     private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setContentView(R.layout.activity_home)
 
         val bundle: Bundle? = intent.extras
         val email = bundle?.getString("email")
 
-        binding.textView2.setText(email).toString()
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = binding.drawerLayout
+
+        toggle = ActionBarDrawerToggle(this,drawer, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
     }
-    fun setDataUserPatient(email: String) {
-        db.collection("Patients").document(email).get().addOnSuccessListener {
-            binding.textView2.setText(it.get("Name") as String?).toString()
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_item_one -> Toast.makeText(this, "Item 1", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_two -> Toast.makeText(this, "Item 2", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_three -> Toast.makeText(this, "Item 3", Toast.LENGTH_SHORT).show()
         }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
